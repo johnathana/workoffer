fde199eb454bf84e5727a2e62df7686eb39a243c
johnathana <johnathana@gmail.com>
2011-11-22 16:46:03 +0200

    Add menu.php file

---
+++ includes/menu.php
@@ -0,0 +1,12 @@
+
+<?php
+	require_once($_SERVER['DOCUMENT_ROOT'].'/includes/auth.php');
+	global $auth;
+?>
+
+<div class="content promos grid2col"> 
+	<aside class="column first" id="optimized">
+		<div><?php echo $auth->email;?></div>
+		<div><p><a href="/logout.php">Αποσύνδεση</a></p></div>
+	</aside> 
+</div>
