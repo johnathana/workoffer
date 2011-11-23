15e75766c4a1e3680d2d47249bf577d818b44421
johnathana <johnathana@gmail.com>
2011-11-23 14:42:28 +0200

    Decorate menu

--- includes/menu.php
+++ includes/menu.php
@@ -4,10 +4,13 @@
 	global $auth;
 ?>
 
-<div class="content promos grid2col"> 
-	<aside class="column first" id="optimized">
-		<div><?php echo $auth->email;?></div>
-		<div><p><a href="/account.php">Επεξεργασία</a></p></div>
-		<div><p><a href="/logout.php">Αποσύνδεση</a></p></div>
-	</aside> 
+<div class="content promos"> 
+	<div class="grid3col">
+		<div class="column first" style="width: 150px">
+			<a href="/account.php"><?php echo $auth->email;?></a>
+		</div>
+		<div class="column last" style="width: 100px">
+			<a href="/logout.php">Αποσύνδεση</a>
+		</div>
+	</div>
 </div>
